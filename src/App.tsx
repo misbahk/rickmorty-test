94d825d1cc8b69523f1e43b82a9e342d04d9cb7f
Misbah Kounain <misbahkounain@Misbahs-MacBook-Air.local>
2025-06-03 16:21:29 +0530

    fix: properly type App component with React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { type FC } from 'react';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <RouterProvider router={router} />
   );
-}
+};
 
 export default App;
